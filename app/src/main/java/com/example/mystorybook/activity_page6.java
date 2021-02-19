5b33e0c55f085c050a50cb2b57403025f4717f40
lummushc <lummushc@s.dcsdk12.org>
2021-02-19 11:46:02 -0700

    Initial release

---
+++ app/src/main/java/com/example/mystorybook/activity_page6.java
@@ -0,0 +1,14 @@
+package com.example.mystorybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_page6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page6);
+    }
+}
